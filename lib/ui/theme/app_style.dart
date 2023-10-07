9caf7fbc2bce1035a229f71f85d81540a2ff2739
Edeson Bizerril <edeson.alves@hotmail.com>
2023-10-06 21:57:58 -0300

    feat: create structure style text theme

---
+++ lib/ui/theme/app_style.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+/// All typografy and style
+class AppStyle {
+  /// Headding 1
+  ///
+  /// Font size: 32px
+  ///
+  ///fontFamily: "Roboto"
+  ///
+  /// Font Weight: SemiBold -- w400
+  ///
+  static final h1 = TextStyle(
+    fontSize: 32,
+    color: AppColors.fontPrimary,
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+  );
+
+  /// Headding 2
+  ///
+  /// Font size: 30px
+  ///
+  ///fontFamily: "Roboto"
+  ///
+  /// Font Weight: Bold -- w400
+  ///
+  static final h2 = TextStyle(
+    fontSize: 30,
+    color: AppColors.fontPrimary,
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+  );
+
+  /// Headding 3
+  ///
+  /// Font size: 30px
+  ///
+  ///fontFamily: "Roboto"
+  ///
+  /// Font Weight: Bold -- w400
+  ///
+  static final h3 = TextStyle(
+    fontSize: 28,
+    color: AppColors.fontPrimary,
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.w400,
+  );
+
+  /// Body
+  ///
+  /// Font size: 12px
+  ///
+  ///fontFamily: "Roboto"
+  ///
+  /// Font Weight: Regular -- normal
+  ///
+  static final body = TextStyle(
+    fontSize: 12,
+    color: AppColors.fontPrimary,
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.normal,
+  );
+
+  /// Body
+  ///
+  /// Font size: 10px
+  ///
+  ///fontFamily: "Roboto"
+  ///
+  /// Font Weight: Semi Bold -- normal
+  ///
+  static final label = TextStyle(
+    fontSize: 10,
+    color: AppColors.fontPrimary,
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.normal,
+  );
+
+  /// Body
+  ///
+  /// Font size: 10px
+  ///
+  ///fontFamily: "Roboto"
+  ///
+  /// Font Weight: Semi Bold -- normal
+  ///
+  static final labelStyle = TextStyle(
+    fontSize: 10,
+    color: AppColors.fontPrimary,
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.normal,
+  );
+}
