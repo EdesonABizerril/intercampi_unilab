e2c9446f14f25d73be3d14b03b0b86aea116604d
Edeson Bizerril <edeson.alves@hotmail.com>
2023-10-08 09:52:39 -0300

    feat: add route to home page after 3 seconds

--- lib/controllers/splash_controller.dart
+++ lib/controllers/splash_controller.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:intercampi/routes/routes.dart';
 import 'package:yaml/yaml.dart';
 
 class SplashController extends GetxController {
   SplashController();
+
+  @override
+  void onInit() {
+    super.onInit();
+    _initAction();
+  }
 
   Future<String> get version => _getVersion();
 
@@ -11,5 +18,12 @@ class SplashController extends GetxController {
     final pubspec = await rootBundle.loadString("pubspec.yaml");
     final yaml = loadYaml(pubspec);
     return yaml["version"].toString();
+  }
+
+  void _initAction() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () => AppNavigator.offNamed.home(),
+    );
   }
 }
