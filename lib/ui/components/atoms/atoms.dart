2cba7f9456b33d0ea80acaa1813ee2cab2fe21c4
Edeson Bizerril <edeson.alves@hotmail.com>
2023-12-03 21:27:08 -0300

    fix: change atmos folder name to atoms name

---
+++ lib/ui/components/atoms/atoms.dart
@@ -0,0 +1,7 @@
+export 'app_bottom_bar_clock.dart';
+export 'app_card_select.dart';
+export 'app_loading.dart';
+export 'app_simple_app_bar.dart';
+export 'app_tab_bar.dart';
+export 'app_time_dashboard.dart';
+export 'app_toast.dart';
