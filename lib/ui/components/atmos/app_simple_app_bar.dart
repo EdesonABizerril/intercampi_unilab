db186b3654ff07e84cf53654cc7fa6295e862bbc
Edeson Bizerril <edeson.alves@hotmail.com>
2023-12-03 21:21:52 -0300

    feat: create app simple app bar component

---
+++ lib/ui/components/atmos/app_simple_app_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../theme/theme.dart';
+
+class AppSimpleAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const AppSimpleAppBar({
+    super.key,
+    required this.title,
+    this.actions = const [],
+    this.backgroundColor,
+    this.textStyle,
+    this.bottom,
+    this.height = kToolbarHeight,
+    this.elevation = 0,
+  });
+
+  final String title;
+  final List<Widget> actions;
+  final Color? backgroundColor;
+  final TextStyle? textStyle;
+  final PreferredSizeWidget? bottom;
+  final double height;
+  final double elevation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title,
+        style: textStyle ??
+            AppStyle.body.copyWith(
+              color: AppColors.white,
+              fontSize: 20,
+            ),
+      ),
+      elevation: elevation,
+      backgroundColor: backgroundColor ?? AppColors.primary,
+      centerTitle: false,
+      actions: actions,
+      bottom: bottom,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight + (bottom?.preferredSize.height ?? 0));
+}
