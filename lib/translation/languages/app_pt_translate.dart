5a821736253c485ea6a580b0d7330534c706d45a
Edeson Bizerril <edeson.alves@hotmail.com>
2023-12-03 21:41:38 -0300

    feat: create translate pt-br texts

--- lib/translation/languages/app_pt_translate.dart
+++ lib/translation/languages/app_pt_translate.dart
@@ -3,4 +3,18 @@
 /// Map for ptBr translation
 final Map<String, String> ptTranslate = {
   "app_catchphrase": "Dinâmico, rápido e prático",
+  "feedback": "Feedback",
+  "about": "Sobre",
+  "shared": "Compartilhe",
+  "shared_text_with_link":
+      """Com o app Intercampi é muito fácil obter os horários dos ônibus de intercampi da UNILAB e da linha pública Fortaleza - Redenção. BAIXE AGORA o app Intercampi e permaneça atualizado! 
+      https://play.google.com/store/apps/details?id=com.edesonabizerril.newintercampi""",
+  "data_updated_in": "Dados atualizados em ##",
+  "select_a_route": "Selecione uma rota",
+  "error_load_route_data": "Falha ao carregar dados da rota",
+  "no_data_found": "Nenhuma dado encontrado",
+  "check_the_departure_times": "Confira os horários de saída",
+  "now": "agora",
+  "x_minutes": "#minmin", // ex: 5 min. Do not translate #min
+  "x_hours": "#hh", // ex: 5 h. Do not translate #h
 };
