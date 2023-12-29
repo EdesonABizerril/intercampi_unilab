5cd18b57f6a6963f28afef52f2a4d09f836b5111
Edeson Bizerril <edeson.alves@hotmail.com>
2023-12-28 23:54:20 -0300

    feat: create images paths

--- lib/ui/assets/app_image.dart
+++ lib/ui/assets/app_image.dart
@@ -1,3 +1,6 @@
 class AppImagePath {
   static String get logoWithName => "lib/ui/assets/images/logo_with_name.png";
+  static String get logoLandscape => "lib/ui/assets/images/logo_landscape.png";
+  static String get linkedinIcon => "lib/ui/assets/images/linkedin.png";
+  static String get githubIcon => "lib/ui/assets/images/github.png";
 }
