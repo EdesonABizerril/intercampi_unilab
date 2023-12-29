f83313faf23a7976b2d9ddaabc1719380191495b
Edeson Bizerril <edeson.alves@hotmail.com>
2023-12-28 23:52:00 -0300

    feat: create about screen

---
+++ lib/ui/screens/about_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../templates/templates.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AboutTemplate();
+  }
+}
--- lib/ui/screens/screens.dart
+++ lib/ui/screens/screens.dart
@@ -1,3 +1,4 @@
+export 'about_screen.dart';
 export 'home_screen.dart';
 export 'route_screen.dart';
 export 'splash_screen.dart';
